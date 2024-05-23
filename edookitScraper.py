8fa30db934468a32d5d7213ae3b217ca3c64c29b
deu42236 <deu42236@spseol.cz>
2024-05-23 16:25:20 +0200

    more lessons

--- edookitScraper.py
+++ edookitScraper.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-import pprint
+
 
 #open https://spseol.edookit.net/
-#inspect -> network -> F5 -> right click on the first request [spseol.edookit.net] -> copy -> copy as cURL(bash)
+#inspect -> network -> right click on the first request [spseol.edookit.net] -> copy -> copy as cURL(bash)
 #copy cURL(bash) and convert
 #https://curlconverter.com/
+
+
+
 
 url = 'https://spseol.edookit.net/timetable/static/'
 
 cookies = {
-
+    
 }
 
 headers = {
@@ -18,20 +21,26 @@ headers = {
 }
 
 
+
+
 response = requests.get(url, cookies=cookies, headers=headers)
 #print(response.status_code)
 # response 200 -> OK, if not big bad
 
 soup = BeautifulSoup(response.content, 'html.parser')
-# lessonsRaw = soup.findAll(attrs={'class':'lesson-info'})
+lessons = soup.findAll(attrs={'class':'lesson-info'})
 lessonRaw = soup.find(attrs={'class':'lesson-info'})
 
-lessonExtract = []
-multiLineLesson = lessonRaw.text.splitlines() #multiline string to list of strings
 
-for i in multiLineLesson:
-    i = i.replace(' ', '')
-    if i != '': #remove empty elements
-        lessonExtract.append(i)
+# print(lessons[0].text.splitlines())
+finalList = [] #matrix of all lessons
 
-print(lessonExtract)
+for j in range(5):
+    multiLineLesson = lessons[j].text.splitlines() #multiline string to list of strings
+    currentLesson = []
+    for i in multiLineLesson:
+        i = i.replace(' ', '')
+        if i != '': #remove empty elements
+            currentLesson.append(i)
+    finalList.append(currentLesson)
+print(finalList) #matrix of all lessons
