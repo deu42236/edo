0bb8f03e45d294f2fd385955e8053dd1e4463a3a
deu42236 <deu42236@spseol.cz>
2024-05-29 16:52:46 +0200

    better formating

--- keys.py
+++
@@ -1,6 +0,0 @@
-#open https://spseol.edookit.net/
-#inspect -> network -> right click on the first request [spseol.edookit.net] -> copy -> copy as cURL(bash)
-#copy cURL(bash) and convert (or cURL, if you use Firefox you will have only this option)
-#https://curlconverter.com/
-#copy the result here
-#don't erase anything, especially the headers{} and cookies{}
