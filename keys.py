f2624c250356845417daee82a30c34dbef7604ae
deu42236 <deu42236@spseol.cz>
2024-05-29 16:26:38 +0200

    added separate file for keys

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 #open https://spseol.edookit.net/
 #inspect -> network -> right click on the first request [spseol.edookit.net] -> copy -> copy as cURL(bash)
-#copy cURL(bash) and convert
+#copy cURL(bash) and convert (or cURL, if you use Firefox you will have only this option)
 #https://curlconverter.com/
 #copy the result here
 #don't erase anything, especially the headers{} and cookies{}
