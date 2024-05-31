32efd79dbe35fb32f48b397713c35d2fe71027c7
deu42236 <deu42236@spseol.cz>
2024-05-31 13:24:23 +0200

    readme file added

---
+++ keys.py
@@ -0,0 +1,9 @@
+#open https://spseol.edookit.net/
+#inspect -> network -> right click on the first request [spseol.edookit.net] -> copy -> copy as cURL(bash)
+#copy cURL(bash) and convert
+"""
+
+https://curlconverter.com/
+"""
+#also keep the browser open and logged in when you run the script
+#paste here
